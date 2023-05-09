50a211bc9028a26b3bdae82aa434470e9c562a1f
牛不会拉屎 <3195759332@QQ.COM>
2023-05-09 21:13:33 +0800

    优化

--- todo.py
+++ todo.py
@@ -3,9 +3,9 @@ import re
 import time
 
 
-def today_todo(today_time, today):
+def today_todo(today_time, path):
 
-    f = open(today)
+    f = open(path)
     text = f.read()
     f.close()
 
@@ -16,34 +16,28 @@ def today_todo(today_time, today):
         return None
 
 
-def new_todo(today, today_time):
+def new_todo(today_time, path):
 
     try:
-        f = open(today, "r")
+        f = open(path, "r")
     except:
-        f = open(today, "w")
+        f = open(path, "w")
         f.close()
-        f = open(today, "r")
+        f = open(path, "r")
 
-    text1 = input("代办: ")
-    if today_todo(today_time, today) is not None:
+    text1 = input("代办:")
+    if today_todo(today_time, path) is not None:
         last_line = f.readlines()[-1]
         index = int(re.findall("(.*)\..*", last_line)[0]) + 1
         f.close()
-        f = open(today, "a")
+        f = open(path, "a")
         f.write("{}.{}\n".format(index, text1))
         f.close()
-        text2 = today_todo(today_time, today)
-        os.system("clear")
-        print(text2)
     else:
         f.close()
-        f = open(today, "a")
+        f = open(path, "a")
         f.write("{}\n\n1.{}\n".format(today_time, text1))
         f.close()
-        text2 = today_todo(today_time, today)
-        os.system("clear")
-        print(text2)
 
 
 def yes_todo(path):
@@ -75,30 +69,76 @@ def yes_todo(path):
     f.close()
                 
 
+def max_num(path):
+    
+    nums = list()
+    f = open(path, "r")
+    lines = f.readlines()
+    f.close()
+    
+    for line in lines:
+        nums.append(len(line))
+    
+    num = max(nums)
+    return num + 12
+
+
+def printf(today_time, path):
+
+    f = open(path, "r")
+    lines = f.readlines()
+    f.close()
+
+    text = today_todo(today_time, path)
+    num = max_num(path)
+    os.system("clear")
+    print("*" * num) 
+    print(text)
+    print("*" * num) 
+    
+
+def input_n_y(command, today_time, path):
+
+    if command == "n" or command == "N":
+        new_todo(today_time, path)
+        printf(today_time, path)
+        return 1
+    elif command == "y" or command == "Y":
+        printf(today_time, path)
+        yes_todo(path)
+        printf(today_time, path)
+        return 1
+    else:
+        print("\n输入有误!")
+        return 0
+
+
 def main():
 
     today = time.localtime(time.time())
     today_time = "{}.{}.{}".format(today[0], today[1], today[2])
     path = "./all_todo_list/%s.txt" % today_time
-    os.system("clear")
+    flag = False
 
-    while True:
-        command = input("今天也是美好的一天(๑•̀ㅂ•́)و✧: ")
+    if os.path.exists(path):
+        printf(today_time, path)
+        flag = True
+    else:
+        command = input("今天还没有代办事项喔, 快来创建一个吧: ")
         if command == "n" or command == "N":
-            new_todo(path, today_time)
-        elif command == "y" or command == "Y":
-            text2 = today_todo(today_time, path)
-            os.system("clear")
-            print(text2)
-            yes_todo(path)
-            text2 = today_todo(today_time, path)
-            os.system("clear")
-            print(text2)
+            if input_n_y(command, today_time, path):
+                print("\n开启新的一天呀~")
+                return
         else:
             print("\n输入有误!")
-            break
-        
-
+            return
+            
+    while True:
+        if flag:
+            command = input("今天也是美好的一天(๑•̀ㅂ•́)و✧: ")
+            if not input_n_y(command, today_time, path):
+                break
+            
 if __name__ == "__main__":
     main()
     
